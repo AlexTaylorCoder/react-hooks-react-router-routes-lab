873eddd195cefbc16914be8daf429198b601fe9b
alextaylor <alextaylor515@gmail.com>
2022-07-03 10:45:35 -0400

    commit tree cf28e5f37c5024647e104d91d15ec4c54bbe9b6e

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,12 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList = directors.map((item,index)=><div key={index}>{item.name}<ul>{item.movies.map((movie,index)=><li key={index}>{movie}</li>)}</ul></div>)
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorList}
+  </div>);
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,12 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie,index)=><div key={index}>{movie.title}{movie.time}<ul>{movie.genres.map((genre,index)=><li key={index}>{genre}</li>)}</ul></div>)
+  return (
+  <div>
+    <h1>Movies Pages</h1>
+    {movieList}
+  </div>);
 }
 
 export default Movies;
